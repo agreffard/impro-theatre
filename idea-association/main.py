2566eab7c085103b452dcc1c3a15912efb5e78f3
Aurélien Greffard <augr@mikrosimage.eu>
2015-10-09 17:47:45 +0200

    Replaced tabs by spaces
    caca!

--- idea-association/main.py
+++ idea-association/main.py
@@ -3,36 +3,36 @@ import random
 import time
 
 with open("words.txt", 'r') as file:
-	words = [ word for word_category in [line.split(',') for line in file.readlines()] for word in word_category]
+    words = [ word for word_category in [line.split(',') for line in file.readlines()] for word in word_category]
 
 waiting_time = 1.2
 
 def wait():
-	time.sleep(waiting_time)
+    time.sleep(waiting_time)
 
 def print_intro():
-	for i in range(3, 0, -1):
-		print(str(i)+"...")
-		wait()
+    for i in range(3, 0, -1):
+        print(str(i)+"...")
+        wait()
 
 def print_word():
-	print(random.choice(words))
-	wait()
-	print("...?")
-	wait()
+    print(random.choice(words))
+    wait()
+    print("...?")
+    wait()
 
 def lets_go_baby():
-	print_intro()
-	while True:
-		print_word()
+    print_intro()
+    while True:
+        print_word()
 
 if __name__ == "__main__":
-	if(len(sys.argv) > 1):
-		try:
-			value = float(sys.argv[1])
-			if(value>0):
-				global waiting_time
-				waiting_time = value
-		except ValueError:
-			pass
-	lets_go_baby()
+    if(len(sys.argv) > 1):
+        try:
+            value = float(sys.argv[1])
+            if(value>0):
+                global waiting_time
+                waiting_time = value
+        except ValueError:
+            pass
+    lets_go_baby()
